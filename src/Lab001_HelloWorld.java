eecd31f7eeda815f2ac366655690f1785f41c6a0
dharitri <dharitri@instasafe.com>
2025-01-05 11:50:31 +0530

    This is intro file

--- src/Lab001_HelloWorld.java
+++ src/Lab001_HelloWorld.java
@@ -1,4 +1,8 @@
-package PACKAGE_NAME;
-
 public class Lab001_HelloWorld {
+
+    public static void main(String[] args){
+
+        System.out.print("Hello World");
+    }
 }
+
---
+++ src/lab002.java
@@ -0,0 +1,6 @@
+public class lab002 {
+
+    public static void main(String[] args) {
+        System.out.println("Hi Dharitri this side!");
+    }
+}
